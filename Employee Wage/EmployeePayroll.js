a5f7e40a3aab1542a3113bf1cab28039a007ff4e
unknown <rachitshukla08@gmail.com>
2020-11-07 18:36:21 +0530

    Added: getters and setters for gender and property for date

--- Employee Wage/EmployeePayroll.js
+++ Employee Wage/EmployeePayroll.js
@@ -1,17 +1,35 @@
 class EmployeePayrollData{
-    id;
-    salary;
-    gender;
     startDate;
 
     //constructor
-    constructor(id,name,salary){
-        this.id = id;
-        this.name = name;
-        this.salary = salary;
+    constructor(...params){
+        this.id = params[0];
+        this.name = params[1];
+        this.salary = params[2];
+        this.gender = params[3];
+        this.startDate = params[4];
     }
 
     //getter and setter
+    get id(){
+        return this._id;
+    }
+    set id(id){
+        console.log("setting id :"+id);
+        this._id = id;
+    }
+    get salary(){
+        return this._salary;
+    }
+    set salary(salary){
+        this._salary = salary;
+    }
+    get gender(){
+        return this._gender;
+    }
+    set gender(gender){
+        this._gender = gender;
+    }
     get name(){
         return this._name;
     }
@@ -21,7 +39,10 @@ class EmployeePayrollData{
 
     //toString
     toString(){
-        return "id = "+this.id+",name = "+this.name+",salary = "+this.salary;
+        const options = {year: 'numeric', month: 'long', day:'numeric'};
+        const empDate = this.startDate == undefined ? "undefined":
+                        this.startDate.toLocaleDateString("en-IN",options);
+        return "id = "+this.id+",name = "+this.name+",salary = "+this.salary +",gender = "+this.gender+",start date = "+empDate;
     }
 }
 
@@ -29,3 +50,6 @@ let employeePayrollData = new EmployeePayrollData(1,"Mark",30000);
 console.log(employeePayrollData.toString());
 employeePayrollData.name = "john";
 console.log(employeePayrollData.toString());
+
+let newEmployeePayrollData = new EmployeePayrollData(1,"Terrisa",30000,"F",new Date());
+console.log(newEmployeePayrollData.toString());
